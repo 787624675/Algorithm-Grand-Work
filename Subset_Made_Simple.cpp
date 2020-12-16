9e9b36315bcb0bffaf327efa0ce00fcf6003265e
linkun <787624675@qq.com>
2020-12-16 22:32:00 +0800

    Update Subset_Made_Simple.cpp

--- Subset_Made_Simple.cpp
+++ Subset_Made_Simple.cpp
@@ -3,6 +3,7 @@
 #include <set>
 #include <math.h>
 #include <algorithm>
+#include <complex>
 
 using namespace std;
 
@@ -11,6 +12,44 @@ using namespace std;
 #define add2(x,y) Add_ltu_2d(x,y)
 #define asss(w,z) All_Subset_Sums_sharp(w,z)
 #define ass(w,z) All_Subset_Sums(w,z)
+#define cp complex<double>
+#define MAXN 30
+#define N 30
+#define fo(x,y,z) for(x = y; x<z+1; x++)
+
+const double pie=acos(-1);
+int n;
+cp a[N],b[N];
+int rev[N],ans[N];
+char s1[N],s2[N];
+void swap1(cp &a, cp &b){
+
+}
+
+void fft(cp *a,int n,int inv)
+{
+    int bit=0;
+    while ((1<<bit)<n)bit++;
+	int i;
+    fo(i,0,n-1)
+    {
+        rev[i]=(rev[i>>1]>>1)|((i&1)<<(bit-1));
+        if (i<rev[i])swap1(a[i],a[rev[i]]);//不加这条if会交换两次（就是没交换）
+    }
+    for (int mid=1;mid<n;mid*=2)//mid是准备合并序列的长度的二分之一
+    {
+    	cp temp(cos(pi/mid),inv*sin(pi/mid));//单位根，pi的系数2已经约掉了
+        for (int i=0;i<n;i+=mid*2)//mid*2是准备合并序列的长度，i是合并到了哪一位
+		{
+            cp omega(1,0);
+            for (int j=0;j<mid;j++,omega*=temp)//只扫左半部分，得到右半部分的答案
+            {
+                cp x=a[i+j],y=omega*a[i+j+mid];
+                a[i+j]=x+y,a[i+j+mid]=x-y;//这个就是蝴蝶变换什么的
+            }
+        }
+    }
+}
 
 int Sum_X(set<int> X){
 	set<int>::iterator it;
